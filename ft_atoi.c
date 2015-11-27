becb8a714ed9b57d13b2e5c7b4f55d19f9441b4e
Cristi <cristi@Cristian-Romeos-MacBook-Pro.local>
2015-11-27 17:49:32 +0100

    deleted extra files, remade atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,3 +1,7 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
 int ft_atoi (const char *str)
 {
         int     i;
@@ -8,23 +12,23 @@ int ft_atoi (const char *str)
         i = 0;
         n = 0;
 
-        while ((str[i] == ' ') || (str[i] == '\t'))
-        {
-        i++;
-                if (str[i] == '-')
-                        neg = 1;
-                if ((str[i] == '-') || (str[i] == '+'))
-                        i++;
-                while(str[i] >= '0' && str[i] <= '9')
-                {
-                        n = n * 10;
-                        n = n + ((int)str[i] - 48);
-                        i++;
-                };
+        while (str[i] == ' ' || str[i] == '\t' || str[i] == '\r' || 
+		str[i] == '\n' || str[i] == '\v' || str[i] == '\f')
+		i++;
+	if (str[i] == '-')
+		neg = 1;
+	if ((str[i] == '-') || (str[i] == '+'))
+		i++;
+	while(str[i] >= '0' && str[i] <= '9')
+	{
+		n = n * 10;
+		n = n + ((int)str[i] - 48);
+		i++;
+	}
         if (neg == 1)
                 return (-n);
         else
                 return (n);
-        }
+        
         return (0);
 }
