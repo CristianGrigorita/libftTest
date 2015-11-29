fe1734614ccbb9467984f4cb2351bec8556c2c79
Cristi <cristi@Cristian-Romeos-MacBook-Pro.local>
2015-11-29 22:33:20 +0100

    libft.h change

--- libft.h
+++ libft.h
@@ -37,5 +37,28 @@ char	*ft_strncpy(char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *s1, const char *s2, size_t n);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strstr(const char *s1, const char *s2);
+void	ft_putchar(char c);
+void	ft_putstr(char const *s);
+void	ft_putendl(char const *s);
+void	ft_putchar_ft(char c, int fd);
+void	ft_putstr_fd(char const *s, int fd);
+void	ft_putendl_fd(char const *s, int fd);
+int	ft_strequ(char const *s1, char const *s2);
+int	ft_strnequ(char const *s1, char const *s2, size_t n);
+void	ft_strclr(char *s);
+void	ft_striter(char *s, void (*f)(char *));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+void	ft_strdel(char **as);
+char	*ft_strnew(size_t size);
+void	ft_memdel(void **ap);
+void	*ft_memalloc(size_t size);
+void	ft_putnbr(int n);
+void	ft_putnbr_fd(int n, int fd);
+char	*ft_strtrim(char const *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
+char	*ft_strmap(char const *s, char (*f)(char));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+
 
 #endif
