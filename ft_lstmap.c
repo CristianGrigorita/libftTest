01e139a58b44aaaec801e6546669c1c5e86da3c6
Cristi <cristi@Cristian-Romeos-MacBook-Pro.local>
2015-12-28 22:02:57 +0100

    delete all old

--- ft_lstmap.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
-{
-	t_list *sth;
-
-	sth = f(lst);
-	return (0);
-}
--- livrare/README.md
+++
@@ -1,5 +0,0 @@
-# LibftFinal
-# LibftFinal
-# LibftFinal
-# LibftFinal
-# LibftFinal
